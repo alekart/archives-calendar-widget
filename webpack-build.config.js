43a71c1d94f4df27f7fa447dc4d94bc721ba311e
alek <alekart@users.noreply.github.com>
2021-12-30 23:59:02 +0100

    Configure webpack to build SCSS files and watch changes on dev

--- webpack-build.config.js
+++ webpack-build.config.js
@@ -1,7 +1,62 @@
 const config = require('./webpack.config');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   ...config,
   mode: 'production',
   devtool: false,
+  watch: false,
+  plugins: [
+    ...config.plugins,
+    new BundleAnalyzerPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              plugins: [
+                ['lodash'],
+              ],
+            },
+          },
+          'ts-loader',
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              // Prefer `dart-sass`
+              implementation: require('sass'),
+              sourceMap: false,
+              sassOptions: {
+                outputStyle: 'compressed',
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimize: false,
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          output: {comments: false},
+        },
+      }),
+    ],
+  },
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,63 @@
 const path = require('path');
+const fs = require('fs');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const themesPath = './src/styles';
+
+function getScssEntries() {
+  const themes = {};
+  fs.readdirSync(themesPath).forEach(file => {
+    if(/^_/.test(file)){
+      return;
+    }
+    const filename = path.basename(file, '.scss');
+    themes[filename] = `${themesPath}/${file}`;
+  });
+  return themes;
+}
 
 module.exports = {
   mode: 'development',
-  entry: './src/scripts/arcw.ts',
+  plugins: [
+    new MiniCssExtractPlugin(),
+  ],
+  entry: {
+    arcw: './src/scripts/arcw.ts',
+    ...getScssEntries(),
+  },
   devtool: 'source-map',
   target: ['web', 'es5'],
+  watch: true,
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: [
+          'ts-loader',
+        ],
         exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              // Prefer `dart-sass`
+              implementation: require('sass'),
+              sourceMap: true,
+            },
+          },
+        ],
       },
     ],
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: 'arcw.js',
     path: path.resolve(__dirname, './src/scripts/dist'),
   },
 };
