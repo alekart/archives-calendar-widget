3ed15c978fb4548f153465a18f76a991c49237a5
alek <alekart@users.noreply.github.com>
2024-11-05 17:16:11 +0100

    Add array utils methods to add and remove simple values

---
+++ src/utils/array-utils.js
@@ -0,0 +1,17 @@
+export function removeFromArray(array, value) {
+  const arrayPos = array.indexOf(value);
+  if (arrayPos === -1) {
+    return [...array];
+  }
+  const updated = [...array];
+  updated.splice(arrayPos, 1);
+  return updated;
+}
+
+export function addUniqueToArray(array, value) {
+  const inArray = array.indexOf(value) !== -1;
+  if (inArray) {
+    return [...array];
+  }
+  return [...array, value];
+}
